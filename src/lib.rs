fa03921749c578eba6e7d3300d01c6697d77b6b5
Pyry Lahtinen <pyry@pyry.info>
2024-11-14 15:26:47 +0200

    parse peer node info from env var

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+#[derive(Debug)]
+pub struct PeerNode {
+    pub id: u64,
+    pub ip_address: String,
+}
 
-pub async fn start_node(node_id: u64, node_list: Vec<String>) {
+pub async fn start_node(node_id: u64, node_list: Vec<PeerNode>) {
     println!("starting node ID={} with peers {:?}", node_id, node_list);
-} 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use ds_project::start_node;
-use std::env::args;
+use ds_project::{start_node, PeerNode};
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    let node_id: u64 = args().nth(1).unwrap().parse().unwrap();
+    let node_id: u64 = env::var("DS_NODE_ID").unwrap().parse().unwrap();
 
-    let other_nodes = args().skip(2).collect();
+    // parse peer node info from comma-separated environment variable
+    // each node is expected contain IP address and node id in format `127.0.0.1#1234`
+    let other_nodes = env::var("DS_PEER_NODES")
+        .unwrap()
+        .split(',')
+        .map(|s| {
+            let parts: Vec<_> = s.split('#').collect();
+            PeerNode {
+                id: parts[1].parse().unwrap(),
+                ip_address: parts[0].to_owned(),
+            }
+        })
+        .collect();
 
     start_node(node_id, other_nodes).await;
 }
