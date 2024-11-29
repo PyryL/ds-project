0ae563e2ef13bb7056acaaebded6f03d35247be3
Pyry Lahtinen <pyry@pyry.info>
2024-11-29 16:46:15 +0200

    fix kv-pair transfer when the node joins the ring

--- src/join.rs
+++ src/join.rs
@@ -1,6 +1,7 @@
 use crate::communication::IncomingConnection;
 use crate::PeerNode;
 use rand::{thread_rng, Rng};
+use std::ops::RangeInclusive;
 
 /// Returns node list and initial leader key-value pairs.
 pub async fn run_join_procedure(known_node_ip_address: &str) -> (Vec<PeerNode>, Vec<(u64, Vec<u8>)>) {
@@ -12,25 +13,22 @@ pub async fn run_join_procedure(known_node_ip_address: &str) -> (Vec<PeerNode>, 
 
     let (smaller_neighbor, greater_neighbor) = find_neighbors(node_id, &node_list);
 
+    let leader_kv_pairs = match (smaller_neighbor, greater_neighbor) {
+        (None, Some(greater)) => request_primary_kv_pairs(&greater, 0..=node_id).await,
+        (Some(smaller), None) => request_primary_kv_pairs(&smaller, (smaller.id+1)..=u64::MAX).await,
+        (Some(smaller), Some(greater)) => request_primary_kv_pairs(&greater, (smaller.id+1)..=node_id).await,
+        (None, None) => Vec::new(),
+    };
+
+    // TODO: request backup key-value pairs
+
+    // TODO: send information to everybody that this node has joined
+
     // add this node itself to the list of nodes
     node_list.push(PeerNode {
         id: node_id,
         ip_address: "127.0.0.1".to_string(),
     });
-
-    let leader_kv_pairs;
-
-    // TODO: handle case where node_list is shorter than 3 but there is still key-value pairs stored
-
-    if node_list.len() >= 3 {
-        // TODO: request backups from greater neighbor
-        // TODO: requets backups from smaller neighbor
-        leader_kv_pairs = request_primary_kv_pairs(node_id, &smaller_neighbor, &greater_neighbor).await;
-    } else {
-        leader_kv_pairs = Vec::new();
-    }
-
-    // TODO: send information to everybody that this node has joined
 
     (node_list, leader_kv_pairs)
 }
@@ -75,46 +73,27 @@ fn ip_address_from_bytes(bytes: &[u8; 4]) -> String {
     bytes.map(|b| b.to_string()).join(".")
 }
 
-/// Parameter node_list may not be empty.
-fn find_neighbors(this_node_id: u64, node_list: &[PeerNode]) -> (PeerNode, PeerNode) {
-    // TODO: check this logic when list is short
-
-    let greatest_node = node_list
-        .iter()
-        .max_by_key(|node| node.id)
-        .unwrap();
-
-    let smallest_node = node_list
-        .iter()
-        .min_by_key(|node| node.id)
-        .unwrap();
-
+/// The neighbors are *not* searched by wrapping around to beginning/end of the ring.
+fn find_neighbors(this_node_id: u64, node_list: &[PeerNode]) -> (Option<PeerNode>, Option<PeerNode>) {
     // smaller neighbor = node with greatest ID smaller than self
-    // if no nodes with smaller ID, use the greatest node
     let smaller_neighbor = node_list
         .iter()
         .filter(|node| node.id < this_node_id)
-        .max_by_key(|node| node.id)
-        .unwrap_or(greatest_node);
+        .max_by_key(|node| node.id);
 
     // greater neighbor = node with smallest ID greater than self
-    // if no nodes with greater ID, use the smallest node
     let greater_neighbor = node_list
         .iter()
         .filter(|node| node.id > this_node_id)
-        .min_by_key(|node| node.id)
-        .unwrap_or(smallest_node);
+        .min_by_key(|node| node.id);
 
-    (smaller_neighbor.clone(), greater_neighbor.clone())
+    (smaller_neighbor.cloned(), greater_neighbor.cloned())
 }
 
-async fn request_primary_kv_pairs(this_node_id: u64, smaller_neighbor: &PeerNode, greater_neighbor: &PeerNode) -> Vec<(u64, Vec<u8>)> {
-    let key_lower_bound = smaller_neighbor.id + 1;
-    let key_upper_bound = this_node_id;
+async fn request_primary_kv_pairs(neighbor: &PeerNode, key_range: RangeInclusive<u64>) -> Vec<(u64, Vec<u8>)> {
+    let request = [vec![11, 0, 0, 0, 21], key_range.start().to_be_bytes().to_vec(), key_range.end().to_be_bytes().to_vec()].concat();
 
-    let request = [vec![11, 0, 0, 0, 21], key_lower_bound.to_be_bytes().to_vec(), key_upper_bound.to_be_bytes().to_vec()].concat();
-
-    let mut connection = IncomingConnection::new(greater_neighbor.ip_address.clone(), &request).await.unwrap();
+    let mut connection = IncomingConnection::new(neighbor.ip_address.clone(), &request).await.unwrap();
 
     let response = connection.read_message().await;
 
